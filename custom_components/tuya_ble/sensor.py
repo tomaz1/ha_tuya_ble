d845b3772bf4ba1f2d9b732a6869a70d1d393f8a
tomaz1 <160363665+tomaz1@users.noreply.github.com>
2024-11-24 02:36:35 +0100

    Update sensor.py

--- custom_components/tuya_ble/sensor.py
+++ custom_components/tuya_ble/sensor.py
@@ -14,7 +14,6 @@ from homeassistant.const import (
     CONCENTRATION_PARTS_PER_MILLION,
     PERCENTAGE,
     SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
-    TEMP_CELSIUS,
     UnitOfVolume,
     UnitOfTemperature,
     UnitOfTime
